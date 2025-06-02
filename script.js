040f6f0488fb25cfda955a24867c3be159e25e89
rene <105558751+renefmelo@users.noreply.github.com>
2025-06-02 17:34:54 -0300

    done: working evaluations
    still has lots of bugs and errors to catch

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const displayText = document.querySelector('.display-text');
+const buttons = document.querySelector('.buttons');
+
+
 function add(num1, num2) {
 	return num1 + num2;
 }
@@ -14,23 +18,99 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
-let num1 = 0;
-let operator = ``;
-let num2 = 0;
+let num1 = null;
+let operator = null;
+let num2 = null;
 
 function operate(num1, operator, num2){
 	switch(operator) {
 		case `+`:
-			add(num1, num2);
+			return add(num1, num2);
 		break;
-		case `-`:
-			subtract(num1, num2);
+		case `−`:
+			return subtract(num1, num2);
 		break;
-		case `*`:
-			multiply(num1, num2);
+		case `∗`:
+			return multiply(num1, num2);
 		break;
-		case `/`:
-			divide(num1, num2);
+		case `÷`:
+			return divide(num1, num2);
 		break;
 	}
 }
+
+let result = 0;
+let pressedOperatorOnce = false;
+let lastPressedIsOp = false;
+let pressedResult = false;
+
+buttons.addEventListener('click', (event) => {
+	let target = event.target;
+	if(target.className === 'number') {
+		if (pressedResult) {
+			displayText.innerText = ``;
+			num1 = null;
+			num2 = null;
+			operator = null;
+			result = 0;
+			pressedOperatorOnce = false;
+			pressedResult = false;
+		}
+		lastPressedIsOp = false;
+		switch (pressedOperatorOnce){
+			case false:
+				if (num1 === null) {
+					num1 = Number(target.innerText);
+					console.log(num1);
+				} else {
+					num1 = Number(`${num1}${target.innerText}`);
+					console.log(num1);
+				}
+			break;
+			default:
+				if (num2 === null){
+					num2 = Number(target.innerText);
+					result = operate(num1, operator, num2);
+					console.log(num2);
+					console.log(`result: ${result}`);
+				} else if (pressedResult){
+					num1 = Number(`${num1}${target.innerText}`)
+					result = num1;
+					console.log(num1);
+				} else {
+					num2 = Number(`${num2}${target.innerText}`);
+					result = operate(num1, operator, num2);
+					console.log(num2);
+					console.log(`result: ${result}`);
+				}
+			break;
+		}
+		displayText.innerText = `${displayText.innerText}${target.innerText}`;
+	} else if (target.className === 'operator') {
+		if (lastPressedIsOp){
+			// removes the operator pressed before this one from the display so it will be
+			// replaced with the new operator pressed.
+			displayText.innerText = displayText.innerText.slice(0, -1);
+		} else {
+			if (pressedOperatorOnce === true){
+				num1 = result;
+				console.log(num1);
+				num2 = null;
+			} else {
+				pressedOperatorOnce = true;
+			}
+			console.log(target.innerText);
+			operator = target.innerText;
+			lastPressedIsOp = true;
+		}
+		displayText.innerText = `${displayText.innerText}${target.innerText}`;
+	} else if (target.className === 'result') {
+		console.log(result);
+		displayText.innerText = result;
+		num1 = result;
+		num2 = null;
+		pressedResult = true;
+	}
+
+})
+//ideia: refazer pegando string do display e dando split com o operador p pegar os numeros e calcular.
